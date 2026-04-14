5729f2d1d77ae7e764a2368893a1bd6613215e4d
michaellakpu <michaella.degerness@student.kpu.ca>
2026-04-14 01:47:38 -0700

    Update login.php

--- login.php
+++ login.php
@@ -60,6 +60,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['role']     = $user['role'];
                 $_SESSION['username'] = $user['username'];
 
+                //---------------------------------------------------------------------
+                //Runs the check for health reports and medications.
+                //Located in login.php to guarantee it will be run if any users are online.
+                date_default_timezone_set("America/Vancouver");
+                $today = date('Y-m-d');
+                //checks if a health report exists. if not, one is made.
+                //specifically: finds any residents who do not have health reports made yet today
+                //and thne makes a health report
+                $stmt = $conn->prepare("SELECT r.residentSIN
+                                        FROM resident r
+                                        WHERE NOT EXISTS 
+                                            (select *
+                                            from healthreport hr
+                                            where date(hr.dateOfCreation) = date(now()) AND r.residentSIN = hr.residentSIN)");
+                $stmt->execute();
+                $reportsList = $stmt->fetchAll();
+                foreach($reportsList as $rl) {
+                            $stmt = $conn->prepare("
+                                    INSERT INTO healthreport
+                                    (residentSIN, empID)
+                                    VALUES (?, 2)");
+                            $stmt->execute([$rl['residentSIN']]);
+                }
+                //checks if a medication entry exists. if not, one is made. entry is linked to daily report
+                //currently report is 2 by default. working on it.
+                $stmt = $conn->prepare("SELECT DISTINCT m.medID,r.fname, max(hr.reportID) AS repID
+                                        FROM medication m
+                                        LEFT JOIN resident r ON m.residentSIN = r.residentSIN
+                                        LEFT JOIN healthreport hr on r.residentSIN = hr.residentSIN
+                                        WHERE NOT EXISTS (SELECT * FROM medication_entry me
+                                                                    where me.date = date(now()) AND m.medID = me.medID)
+                                        GROUP BY m.medID");
+                $stmt->execute();
+                $medsList = $stmt->fetchAll();
+                foreach($medsList as $ml) {
+                            $stmt = $conn->prepare("
+                                    INSERT INTO medication_entry
+                                    (medID, reportID, date)
+                                    VALUES (?, ?, ?)");
+                            $stmt->execute([$ml['medID'],$ml['repID'],$today]);
+                }
+                //-------------------------------------------------------------------
+
                 switch($user['role']) {
                     case 'admin':
                         header("Location: admin/dashboard.php");
